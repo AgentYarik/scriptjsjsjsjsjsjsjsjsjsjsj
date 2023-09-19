707acfd7d4d78c3079c59a2f73f35c4d8763fba7
AgentYarik <125293714+AgentYarik@users.noreply.github.com>
2023-09-19 18:01:44 +0300

    sss

---
+++ script.js
@@ -0,0 +1,35 @@
+/* let myName = "Oliver"
+console.log(myName)
+
+let numberMy = 23
+let numberSend = '12'
+let trueNofalse = true
+console.log(typeof numberMy + typeof numberSend , typeof trueNofalse)  */
+
+/* let infoMy = "Сьогодні був ужасний день"
+alert(infoMy)
+
+confirm("Чи не правда?")
+prompt("") */
+/* 
+let loginName = prompt("Як тебе звати?")
+alert("Вітаємо тебе " + loginName) */
+
+
+/* const inputName = prompt("Будь ласка введіть ваше ім'я:")
+let inputNoname = "Так вивчаємо";
+let chatGpt = "Ми вивчаємо чава скрипт"
+confirm(chatGpt)
+alert(inputName) */
+
+/* let myAge = 12
+console.log(myAge) */
+
+/* let myNames = "Yaroslav"
+console.log(myNames) */
+
+/* let isStudent = true;
+console.log(isStudent) */
+
+/* let myString = "Ramstein, I see you again";
+console.log(myString) */
